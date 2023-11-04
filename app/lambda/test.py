e9336cc370e57b3f3284a925fcd9e4c86b339402
Gabriel Monter <gasamoma@amazon.com>
2023-11-04 15:32:22 +0000

    adding STEM Styles

---
+++ app/lambda/test.py
@@ -0,0 +1,25 @@
+import json
+
+a = {"style": "36",
+  "Records": [
+    {
+      "s3": {
+        "bucket": {
+          "name": "bedrockavatarcreatorstac-bedrockavatarimagestore7-14hmtn4fhp02r"
+        },
+        "object": {
+          "key": "FL1.jpeg"
+        }
+      }
+    }
+  ]
+}
+r_json = list()
+images = ['FL1.jpeg','FL2.jpeg','FL3.jpeg','FL4.jpeg']
+for image in images:
+    for i in range(1,27):
+        r_json.append({"style": str(i), "Records": [{"s3": {"bucket": {"name": "bedrockavatarcreatorstac-bedrockavatarimagestore7-14hmtn4fhp02r"}, "object": {"key": image}}}]})
+# write the json in a file
+with open('data.json', 'w') as outfile:
+    json.dump(r_json, outfile)
+    outfile.close()
---
+++ app/lambda/transfortm_prompts.py
@@ -0,0 +1,124 @@
+a = {
+  "STEMProfessions": {
+    "Science": [
+      {
+        "name": "Biologist",
+        "prompt": "Woman, inquisitive expression, biologist, laboratory setting, microscope, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, biological research, analyzing pose, laboratory tools in hands, curious, Hyper realistic."
+      },
+      {
+        "name": "Chemist",
+        "prompt": "Woman, focused expression, chemist, chemical laboratory, test tubes, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, chemical analysis, experimenting pose, lab equipment in hands, precise, Hyper realistic."
+      },
+      {
+        "name": "Physicist",
+        "prompt": "Woman, contemplative expression, physicist, research facility, particle accelerator, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, theoretical physics, pondering pose, scientific equipment in hands, analytical, Hyper realistic."
+      },
+      {
+        "name": "Astronomer",
+        "prompt": "Woman, awe-struck expression, astronomer, observatory setting, telescope, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, celestial observation, stargazing pose, telescope in hands, fascinated, Hyper realistic."
+      },
+      {
+        "name": "Geologist",
+        "prompt": "Woman, adventurous expression, geologist, fieldwork setting, rock formations, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, geological exploration, examining pose, geological tools in hands, determined, Hyper realistic."
+      },
+      {
+        "name": "Ecologist",
+        "prompt": "Woman, environmentalist expression, ecologist, natural habitat setting, wildlife observation, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, ecological study, observing pose, environmental tools in hands, passionate, Hyper realistic."
+      },
+      {
+        "name": "Microbiologist",
+        "prompt": "Woman, focused expression, microbiologist, laboratory setting, petri dish, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, microbiological research, examining pose, laboratory tools in hands, meticulous, Hyper realistic."
+      },
+      {
+        "name": "Neuroscientist",
+        "prompt": "Woman, thoughtful expression, neuroscientist, brain research lab, EEG equipment, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, neuroscience study, analyzing pose, scientific tools in hands, thoughtful, Hyper realistic."
+      },
+      {
+        "name": "Geneticist",
+        "prompt": "Woman, analytical expression, geneticist, DNA research lab, double helix model, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, genetic research, studying pose, laboratory tools in hands, analytical, Hyper realistic."
+      },
+      {
+        "name": "Environmental Scientist",
+        "prompt": "Woman, environmentally conscious expression, environmental scientist, fieldwork setting, ecological monitoring, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, environmental research, monitoring pose, scientific tools in hands, conscientious, Hyper realistic."
+      },
+    ],
+    "Technology": [
+      {
+        "name": "Software Developer",
+        "prompt": "Woman, focused expression, software developer, coding environment, computer setup, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, coding expertise, coding pose, laptop in hands, innovative, Hyper realistic."
+      },
+      {
+        "name": "Data Scientist",
+        "prompt": "Woman, analytical expression, data scientist, analyzing data, visualizations, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, data analysis, interpreting pose, analytics tools in hands, precise, Hyper realistic."
+      },
+      {
+        "name": "Network Engineer",
+        "prompt": "Woman, determined expression, network engineer, server room, networking equipment, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, network management, configuring pose, networking tools in hands, focused, Hyper realistic."
+      },
+      {
+        "name": "Cybersecurity Analyst",
+        "prompt": "Woman, vigilant expression, cybersecurity analyst, security operations center, monitoring screens, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, cybersecurity, monitoring pose, security tools in hands, alert, Hyper realistic."
+      },
+      {
+        "name": "Database Administrator",
+        "prompt": "Woman, organized expression, database administrator, database management, SQL queries, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, database administration, organizing pose, database tools in hands, efficient, Hyper realistic."
+      },
+      {
+        "name": "UX/UI Designer",
+        "prompt": "Woman, creative expression, UX/UI designer, design studio setting, wireframes, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, user experience design, designing pose, design tools in hands, creative, Hyper realistic."
+      },
+      {
+        "name": "IT Project Manager",
+        "prompt": "Woman, leadership expression, IT project manager, project management office, Gantt chart, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, project management, overseeing pose, project documents in hands, decisive, Hyper realistic."
+      }
+    ],
+    "Engineering": [
+      {
+        "name": "Civil Engineer",
+        "prompt": "Woman, confident expression, civil engineer, construction site, blueprint, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, civil engineering, planning pose, construction tools in hands, determined, Hyper realistic."
+      },
+      {
+        "name": "Mechanical Engineer",
+        "prompt": "Woman, hands-on expression, mechanical engineer, workshop setting, machinery, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, mechanical engineering, designing pose, mechanical tools in hands, skilled, Hyper realistic."
+      },
+      {
+        "name": "Electrical Engineer",
+        "prompt": "Woman, focused expression, electrical engineer, electronics lab, circuit board, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, electrical engineering, testing pose, electronic tools in hands, precise, Hyper realistic."
+      },
+      {
+        "name": "Aerospace Engineer",
+        "prompt": "Woman, determined expression, aerospace engineer, aircraft hangar, designing aircraft, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, aerospace engineering, analyzing pose, aircraft tools in hands, innovative, Hyper realistic."
+      },
+      {
+        "name": "Biomedical Engineer",
+        "prompt": "Woman, compassionate expression, biomedical engineer, medical research lab, biomedical devices, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, biomedical engineering, researching pose, medical tools in hands, empathetic, Hyper realistic."
+      },
+      {
+        "name": "Environmental Engineer",
+        "prompt": "Woman, environmentally conscious expression, environmental engineer, eco-friendly project, sustainable solutions, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, environmental engineering, planning pose, eco-friendly tools in hands, conscientious, Hyper realistic."
+      }
+    ],
+    "Mathematics": [
+      {
+        "name": "Mathematician",
+        "prompt": "Woman, analytical expression, mathematician, mathematical research, equations, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, mathematical exploration, pondering pose, mathematical tools in hands, insightful, Hyper realistic."
+      },
+      {
+        "name": "Statistician",
+        "prompt": "Woman, analytical expression, statistician, statistical analysis, graphs, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, statistical research, analyzing pose, statistical tools in hands, precise, Hyper realistic."
+      },
+      {
+        "name": "Cryptographer",
+        "prompt": "Woman, cryptic expression, cryptographer, encryption algorithms, codebreaking, Canon 5D Mark IV, professional photorealistic scenario, Realistic, proportioned, Cinematic, cryptography, decoding pose, cryptographic tools in hands, cryptic, Hyper realistic."
+      }
+    ]
+  }
+}
+
+promts= {}
+index = 1
+for stem, professions in a['STEMProfessions'].items():
+    for profession in professions:
+        promts[index] = profession
+        index+=1
+print(promts)
